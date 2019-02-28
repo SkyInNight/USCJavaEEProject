c7bbd661540b40afb5d318062d537ddc9fb6155e
NoBugNoLife <1119886064@qq.com>
2019-02-28 17:58:15 +0800

    数据库连接模块完成

--- src/main/java/cn.org.cerambycidae.Servlet/HelloWorldServlet.java
+++ src/main/java/cn.org.cerambycidae.Servlet/HelloWorldServlet.java
@@ -1,12 +1,14 @@
 package cn.org.cerambycidae.Servlet;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
+
 //Servlet访问路径和初始化页面参数
 @WebServlet(
-        urlPatterns = {"/MyFirstServlet"},
-        initParams = {@WebInitParam(name = "language",value="english")}
+        urlPatterns = {""},
+        initParams = {@WebInitParam(name = "language", value = "english")}
 )
 public class HelloWorldServlet extends HttpServlet {
 
@@ -17,20 +19,19 @@ public class HelloWorldServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         //获取初始化对象参数
         String language = this.getInitParameter("language");
-        //
-            if (language != null){
-                if (language.equals("chinese")){
-                     out.println("你好世界。MyFirstServlet"+"<br>");
-                }else{
-                    out.println("Hello World! MyFirstServlet"+"<br>");
-                }
-            }else{
-                out.println("RunTime Error!!"+"<br>");
+        if (language != null) {
+            if (language.equals("chinese")) {
+                out.println("你好世界。MyFirstServlet" + "<br>");
+            } else {
+                out.println("Hello World! MyFirstServlet" + "<br>");
             }
-            out.println(request.getMethod());
+        } else {
+            out.println("RunTime Error!!" + "<br>");
+        }
+        out.println(request.getMethod());
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        this.doPost(request,response);
+        this.doPost(request, response);
     }
 }
---
+++ src/main/java/cn/org/cerambycidae/service/ExampleService.java
@@ -0,0 +1,53 @@
+package cn.org.cerambycidae.service;
+
+import cn.org.cerambycidae.pojo.Student;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/*
+ * ExampleService主要封装对数据库表的基本操作<操作对象，对象主键，对象Example>
+ */
+public interface ExampleService<T, K, O> {
+
+    /* 数据增加 */
+
+    //添加一个数据库记录
+    int insert(T example);
+
+    //异步添加一个数据库记录
+    int insertSelective(T example);
+
+    /* 数据修改 */
+
+    //异步更新数据
+    int updateByExampleSelective(@Param("example") T example, @Param("eexample") O eexample);
+
+    //更新数据
+    int updateByExample(@Param("example") T example, @Param("eexample") O eexample);
+
+    //按照主键异步更新数据
+    int updateByPrimaryKeySelective(T example);
+
+    //按照主键更新数据
+    int updateByPrimaryKey(T example);
+
+    /* 数据查询 */
+
+    //删除数据集
+    int deleteByExample(O eexample);
+
+    //通过主键删除数据
+    int deleteByPrimaryKey(K id);
+
+    /* 数据删除 */
+
+    //查询数据
+    List<T> selectByExample(O eexample);
+
+    //通过主键查询数据
+    Student selectByPrimaryKey(K id);
+
+    /* 统计数据 */
+    long countByExample(O eexample);
+}
